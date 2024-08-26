ccf5f36b8f7417a67f4bbbd26e6a4e72c0788bf8
mishra06 <mishra.vibhakar7262@gmail.com>
2024-08-26 10:27:22 +0530

    "tyre scroll section updated"

--- src/components/popularTyre/PopularTyre.js
+++ src/components/popularTyre/PopularTyre.js
@@ -1,57 +1,83 @@
-import React from 'react'
-import JK from "../../assets/JK-Tyre-logo-PNG-Transparent.png"
-import CEAT from "../../assets/ceat-logo.png"
+import React from "react";
+import JK from "../../assets/JK.png";
+import CEAT from "../../assets/ceat-logo.png";
+import Apollo from "../../assets/apollo-logo.png"
+import Bridg from "../../assets/bridgestone.png";
+import LAufenn from "../../assets/laufenn-logo.png";
+import Mrf from "../../assets/mrf.png";
+import Pire from "../../assets/Pirelli-logo.png";
+import Mich from "../../assets/michelin.png"
+import Yoko from "../../assets/yokohma.png"
 
 const PopularTyre = () => {
-
-    const tyrs = [
-        {
-            name: 'JK Tyres',
-            img : JK
-        },
-        {
-            name: 'CEAT',
-            img : CEAT
-        },
-        {
-            name: 'CEAT',
-            img : CEAT
-        },
-        {
-            name: 'CEAT',
-            img : CEAT
-        },
-        {
-            name: 'CEAT',
-            img : CEAT
-        },
-        {
-            name: 'CEAT',
-            img : CEAT
-        },
-       
-    ]
+  const tyrs = [
+    {
+      name: "JK Tyres",
+      img: JK,
+    },
+    {
+      name: "CEAT",
+      img: CEAT,
+    },
+    {
+      name: "APOLLO",
+      img: Apollo,
+    },
+    {
+      name: "Goodyear",
+      img: "https://www.carlogos.org/tire-logos/goodyear-logo-3000x900-show.png",
+    },
+    {
+      name: "LAUFENN",
+      img: LAufenn,
+    },
+    {
+      name: "MRF",
+      img: Mrf,
+    },
+    {
+      name: "MICHELIN",
+      img: "https://michelinmedia.com/site/user/images/Michelin_Corporate_Logo___color.jpg",
+    },
+    {
+      name: "YOKOHMA",
+      img: Yoko,
+    },
+    {
+      name: "PIRELLI",
+      img: Pire,
+    },
+    {
+      name: "BRIDGESTON",
+      img: "https://www.bridgestoneamericas.com/content/dam/corpcomm/americas/assets/media-center/logos/bridgestone-logos/bridgestone-mark-logos/album-cover.png",
+    },
+  ];
 
   return (
-    <div className='h-64 w-full border-2 border-solid border-black-500 '>
-      <div className='flex justify-start items-center'>
-        <p className='font-medium px-2 py-2'>Popular Tyre Brands</p>
+    <div className="h-64 w-full border-2 border-solid border-black-500 ">
+      <div className="flex justify-start items-center">
+        <p className="font-medium px-2 py-2">Popular Tyre Brands</p>
       </div>
-      <div className='categorycontainer flex justify-center w-95 flex-row gap-4 overflow-x-scroll border-red-600 mt-3' >
-        {
-            tyrs.map((t, i) => (
-              <div key={i} className=' w-40 flex flex-col items-center justify-around h-26 border-2 border-solid border-black-500'>
-                <img className=' px-2 py-2 h-12 ' src={t.img} alt={t.name} />
-                <div className='h-12 text-gray-500'>{t.name}</div>
-              </div>
-            ))
-        }
+      <div className="card-container w-full max-h-64">
+        <div className="categorycontainer flex overflow-x-scroll w-full whitespace-nowrap mt-3">
+          {tyrs.map((t, i) => (
+            <div
+              key={i}
+              className="cursor-pointer card inline-block min-w-[150px] h-24 border border-gray-300 rounded-lg p-4 m-2 flex flex-col text-center justify-center items-center border-2 border-solid border-black-500"
+            >
+              <img className=" h-20 w-20 " src={t.img} alt={t.name} />
+              <div className="h-10 text-gray-500">{t.name}</div>
+            </div>
+          ))}
+        </div>
       </div>
-      <div className='flex justify-center items-center mt-8 mb-2'>
-        <p className='text-blue-800 font-medium'>Car Tyres ||  Bike Tyres ||  Scooter Tyres || Commercial Tyres</p>
+      <div className="flex justify-center items-center mt-8 mb-2">
+        <p className="text-blue-800 font-medium">
+          Car Tyres || Bike Tyres || Scooter Tyres || Commercial Tyres
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PopularTyre
+export default PopularTyre;
